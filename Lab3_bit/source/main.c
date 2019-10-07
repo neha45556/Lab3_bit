0e6f350ff9541d899ed3f5ed025456dd199945d8
neha45556 <neha45556@gmail.com>
2019-10-07 13:22:13 -0700

    Update main.c

--- Lab3_bit/source/main.c
+++ Lab3_bit/source/main.c
@@ -12,39 +12,37 @@
 #include "simAVRHeader.h"
 #endif
 
-unsigned char GetBit(unsigned char x, unsigned char k){
-    return ((x & (0x01 << k)) != 0);
- }
+
         
 
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
-    DDRB = 0x00; PORTB = 0xFF;
     DDRC = 0xFF; PORTC = 0x00;
-    unsigned char i;
-    unsigned char cnt = 0;
-    unsigned char A = 0x00;
-    unsigned char B = 0x00;
-   
+    
+   unsigned char A = 0x00;
+    unsigned char C = 0x00; 
 
-    /* Insert your solution below */
-    while (1) {
-       A = PINA;
-       B = PINB;
-       cnt = 0;
-       for(i = 0; i < 8; i++){
-           
-           if(GetBit(A,i)){
-               cnt++;
-           }
-           
-           if(GetBit(B,i)){
-               cnt++;
-           }
-          
-        }
-        PORTC =cnt; 
+    while(1){
+        A = PINA; //input PINA all of A
+
+    if(A == 0x01 || A = 0x02){
+        C = 0x20;
+    }
+    else if(A == 0x03 || A =0x04){
+        C = 0x30;
+    }
+    else if(A == 0x05 || A == 0x06){
+        C = 0x38;
+    }
+    else if(A == 0x07 || A == 0x08 || A == 0x09){
+        C = 0x3C;
+    }
+    else if(A == 0x0A || A == 0x0B || A == 0x0C){
+        C = 0x3E;
+    }
+     else if(A == 0x0D || A == 0x0E || A == 0x0F){
+        C = 0x3F;
     }
     return 1;
 }
