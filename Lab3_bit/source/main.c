5611302908b14eb0d43f877bc7848f9985ce0f07
neha45556 <neha45556@gmail.com>
2019-10-08 16:32:04 -0700

    Update main.c

--- Lab3_bit/source/main.c
+++ Lab3_bit/source/main.c
@@ -15,42 +15,25 @@
 
         
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRA = 0x00; PORTA = 0xFF;
-    DDRC = 0xFF; PORTC = 0x00;
-    
-   unsigned char A = 0x00;
-    unsigned char C = 0x00; 
+int main(void){
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xFF; PORTC = 0x00;
 
-    while(1){
-        A = PINA & 0x0F; //input PINA all of A
+unsigned char A = 0x00;
+unsigned char B = 0x00;
+unsigned char C = 0x00;
+
+while(1){
+	A = PINA;
+	
+	B = ( A & 0x0F ) >> 4 ;
+	C = ( A & 0xF0) << 4;
+	
+	PORTB = B;
+	PORTC = C;
         
-    
-
-    if(A == 0x01 || A == 0x02){
-        C = 0x60;
-    }
-    else if(A == 0x03 || A == 0x04){
-        C = 0x70;
-    }
-    else if(A == 0x05 || A == 0x06){
-        C = 0x38;
-    }
-    else if(A == 0x07 || A == 0x08 || A == 0x09){
-        C = 0x3C;
-    }
-    else if(A == 0x0A || A == 0x0B || A == 0x0C){
-        C = 0x3E;
-    }
-     else if(A == 0x0D || A == 0x0E || A == 0x0F){
-        C = 0x3F;
-    }
-    if((PINA >> 4) == 0x03){
-          C = C | 0x80;
-    }
-      PORTC = C;
-    }
-    return 1;
-    
+        return 1; 
+     }
 }
+
